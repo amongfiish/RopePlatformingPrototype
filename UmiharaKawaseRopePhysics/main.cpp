77b47a78fac0ef3224b120f17b85c8f3a7971905
Luke James <ljgecko1230@gmail.com>
2023-04-04 22:07:03 -0700

    Initial Commit

---
+++ UmiharaKawaseRopePhysics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UmiharaKawaseRopePhysics
+//
+//  Created by Lucas James on 2023-04-04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
